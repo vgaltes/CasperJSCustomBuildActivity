65a39c5111cd8d3fe5d808f478d10c19353f64f8
Vicenç García Altés <vincentredrum@gmail.com>
2013-03-25 00:07:13 +0100

    TrxCreator with empty results

---
+++ CustomBuildActivities/CasperJsTestsResults.cs
@@ -0,0 +1,25 @@
+﻿namespace CustomBuildActivities
+{
+    using System.Collections.Generic;
+
+    public class CasperJsTestsResults
+    {
+        public IList<CasperJsTest> PassedTests
+        {
+            get;
+            set;
+        }
+
+        public IList<CasperJsTest> FailedTests
+        {
+            get;
+            set;
+        }
+
+        public CasperJsTestsResults()
+        {
+            PassedTests = new List<CasperJsTest>();
+            FailedTests = new List<CasperJsTest>();
+        }
+    }
+}
